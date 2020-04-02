9d9afb1d53078452207e62011c1b78110b3efd07
DLizr <DmitryLizr@yandex.ru>
2020-04-02 22:05:41 +0300

    Simple user tracking system.

--- gameserver/Main.py
+++ gameserver/Main.py
@@ -1,1 +1,22 @@
-print("Hello, TCP Connections!")  # Aka sockets go here.
+import asyncio
+import websockets
+
+
+users = set()
+
+
+async def userHandler(websocket: websockets.WebSocketClientProtocol, _):  # _ is path.
+    print(websocket.remote_address, "has connected.")
+    while True:
+        try:
+            await websocket.send("Test!")
+        except websockets.exceptions.ConnectionClosedOK:
+            print(websocket.remote_address, "has disconnected.")
+            break
+        await asyncio.sleep(1)
+
+
+server = websockets.serve(userHandler, "localhost", 31666)
+
+asyncio.get_event_loop().run_until_complete(server)
+asyncio.get_event_loop().run_forever()
