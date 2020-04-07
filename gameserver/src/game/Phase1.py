3d64b76b786103e5850cb338ec837fd537abbbc0
DLizr <DmitryLizr@yandex.ru>
2020-04-07 12:21:38 +0300

    Fixed kostyli.

--- gameserver/src/game/Phase1.py
+++ gameserver/src/game/Phase1.py
@@ -9,12 +9,12 @@ class Phase1:
     def __init__(self, player1: Player, player2: Player):
         self.__player1 = player1
         self.__player2 = player2
-        self.M = 10
-        self.N = 10
+        self.__M = 10
+        self.__N = 10
 
         self.__time = 30
-        self.__base1 = None
-        self.__base2 = None
+        self.__base1 = [[None for _ in range(self.__N)] for _ in range(self.__M)]
+        self.__base2 = [[None for _ in range(self.__N)] for _ in range(self.__M)]
 
     async def start(self):
         await Messages.startPhase1(self.__player1)
@@ -24,17 +24,33 @@ class Phase1:
             await self.__player1.sendMessage("TIME:{}".format(str(self.__time - i)))
             await self.__player2.sendMessage("TIME:{}".format(str(self.__time - i)))
             await asyncio.sleep(1)
+        
+        self.__getBases()
 
-    async def getBases(self):
-        for i in range(self.M * self.N):
-            msg = self.__player1.getMessageIfReceived()
-            if msg == 'OK':
-                return
-            await self.__player1.ping()
-            await asyncio.sleep(1)
-        for i in range(self.M * self.N):
-            msg = self.__player2.getMessageIfReceived()
-            if msg == 'OK':
-                return
-            await self.__player2.ping()
-            await asyncio.sleep(1)
+    async def __getBases(self):
+        Messages.getBase(self.__player1)
+        
+        for i in range(self.__M * self.__N):
+            for _ in range(2):
+                msg = self.__player1.getMessageIfReceived()
+                if msg:
+                    break
+                
+                await self.__player1.ping()
+                await asyncio.sleep(1)
+                
+            self.__base1[i//self.__M][i%self.__M] = msg
+            
+            
+        Messages.getBase(self.__player2)
+        
+        for i in range(self.__M * self.__N):
+            for _ in range(2):
+                msg = self.__player2.getMessageIfReceived()
+                if msg:
+                    break
+                
+                await self.__player2.ping()
+                await asyncio.sleep(1)
+                
+            self.__base2[i//self.__M][i%self.__M] = msg
--- gameserver/src/player/Messages.py
+++ gameserver/src/player/Messages.py
@@ -50,3 +50,23 @@ class Messages:
         logger.error("{} received a PHASE_1 signal but didn't respond!".format(player.getAddress()))
         await player.disconnect()
         raise PlayerKickedException(player, "No response")
+    
+    @staticmethod
+    async def getBase(player: Player):
+        
+        player.clearMessages()
+        await player.sendMessage("GET_BASE")
+        try:
+            for _ in range(5):
+                msg = player.getMessageIfReceived()
+                if msg == "OK":
+                    return
+                await player.ping()
+                await asyncio.sleep(1)
+        except ConnectionClosedOK as e:
+            e.player = player
+            raise e
+        
+        logger.error("{} received a GET_BASE signal but didn't respond!".format(player.getAddress()))
+        await player.disconnect()
+        raise PlayerKickedException(player, "No response")
